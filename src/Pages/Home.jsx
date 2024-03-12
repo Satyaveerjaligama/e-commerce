1d22a1e556a6e8cbf445ab7aca2e5e89a041d22b
Satyaveerjaligama <satyaveerjaligama26@gmail.com>
2024-03-13 01:11:02 +0530

    Added tailwindcss, phone images, made changes in components

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,23 +1,26 @@
 import React from "react";
-import { Button, Typography } from "@mui/material";
-import Header from "../Components/Header";
+import { Button, Grid, Typography } from "@mui/material";
 import { useSelector, useDispatch } from "react-redux";
 import { increment, decrement } from "../Redux/slices/sampleSlice";
+import Layout from "../Components/Layout";
+import ProductCard from "../Components/ProductCard";
+import { productsList } from "../Utilities/Constants";
 
 const Home = () => {
     const dispatch = useDispatch();
-    const value = useSelector((state)=>state.sampleSlice.counter);
     return (
-        <React.Fragment>
-            <Header />
-            <Typography>{value}</Typography>
-            <Button variant="outlined" onClick={()=>dispatch(increment())}>
-                Increment
-            </Button>
-            <Button variant="outlined" onClick={()=>dispatch(decrement())}>
-                Decrement
-            </Button>
-        </React.Fragment>
+        <Layout>
+            <Grid className="p-5" container rowSpacing={2} columnSpacing={2}>
+                {productsList.map((product)=>
+                    <Grid item xs={12} sm={3} md={2.4}>
+                        <ProductCard productImage={product.productImage} productName={product.productName} productPrice={product.productPrice}/>
+                    </Grid>
+                )}
+            </Grid>
+            {/* <ProductCard />
+            <ProductCard />
+            <ProductCard /> */}
+        </Layout>
     )
 };
 
